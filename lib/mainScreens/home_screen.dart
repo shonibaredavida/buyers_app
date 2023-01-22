c607586d3cb701c3e150579d6b2987cc0105f3f7
shodav4real <shonibaredavida@yahoo.com>
2023-01-22 04:34:49 +0100

    completed

--- lib/mainScreens/home_screen.dart
+++ lib/mainScreens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import "package:flutter/material.dart";
+import 'package:trial/global/global.dart';
 import 'package:trial/widgets/my_drawer.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,6 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       drawer: const MyDrawer(),
       appBar: AppBar(
         title: const Text(
@@ -36,8 +39,54 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: const CircleAvatar(
-        radius: 65,
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.3,
+                width: MediaQuery.of(context).size.width,
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    height: MediaQuery.of(context).size.height * 0.9,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.8,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(seconds: 3),
+                    autoPlayAnimationDuration:
+                        const Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.3,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                  items: itemsImagesList.map((index) {
+                    return Builder(builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 0.5,
+                          vertical: 1,
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(40),
+                          child: Image.asset(
+                            index,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      );
+                    });
+                  }).toList(),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
