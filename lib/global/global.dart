0d1fa6959e60b3ccea43b34b55456afed7ba9e49
shodav4real <shonibaredavida@yahoo.com>
2023-01-22 04:34:49 +0100

    completed

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -1,3 +1,20 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 SharedPreferences? sharedPreferences;
+
+final itemsImagesList = [
+  "slider/0.jpg",
+  "slider/1.jpg",
+  "slider/2.jpg",
+  "slider/3.jpg",
+  "slider/4.jpg",
+  "slider/5.jpg",
+  "slider/6.jpg",
+  "slider/7.jpg",
+  "slider/8.jpg",
+  "slider/9.jpg",
+  "slider/10.jpg",
+  "slider/11.jpg",
+  "slider/12.jpg",
+  "slider/13.jpg",
+];
