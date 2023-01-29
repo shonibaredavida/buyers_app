ec69bf79877c1d5e8ae86e6d89e3b582a255038d
shodav4real <shonibaredavida@yahoo.com>
2023-01-29 20:46:36 +0100

    Done

--- lib/assistant_method/assistant_methods.dart
+++ lib/assistant_method/assistant_methods.dart
@@ -44,4 +44,27 @@ class CartMethods {
           .showCartListItemsNumber();
     });
   }
+
+  separateItemIDsFromUserCartList() {
+    List<String>? userCartList = sharedPreferences!.getStringList("userCart");
+    List<String>? itemIDList = []; //cart will be [123443:4,2323443:1,12345:43]
+    //  if (dev) print(userCartList);
+    for (int i = 1; i < userCartList!.length; i++) {
+      String item = userCartList[i]
+          .toString(); //this will get items in the cart as 122334:4
+
+      var getItemIDonly = item.split(':')[
+          0]; //this will split the items n select everything b4 ":" i.e 122334
+      itemIDList.add(getItemIDonly);
+      /* 
+      //another way
+        var lastCaracterPositionOfItemBeforeColon = item.lastIndexOf(":");
+      String getItemIdOnly =
+          item.substring(0, lastCaracterPositionOfItemBeforeColon);
+      itemIDList.add(getItemIdOnly); */
+      //   if (dev) print("$itemIDList ...... $i");
+    }
+
+    return itemIDList;
+  }
 }
