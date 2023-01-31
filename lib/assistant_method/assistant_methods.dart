e40cf1d12581f17f65044348db5121d806df0d7e
shodav4real <shonibaredavida@yahoo.com>
2023-01-31 04:46:45 +0100

    done

--- lib/assistant_method/assistant_methods.dart
+++ lib/assistant_method/assistant_methods.dart
@@ -27,6 +27,7 @@ class CartMethods {
     //adding the new item to local device
     sharedPreferences!.setStringList("userCart", tempList);
     if (dev) print("WE WE WE WE stored locally");
+
   }
 
   clearCart(BuildContext context) {
@@ -66,5 +67,28 @@ class CartMethods {
     }
 
     return itemIDList;
+  }
+
+  separateItemQtyFromUserCartList() {
+    List<String>? userCartList = sharedPreferences!.getStringList("userCart");
+    List<String>? itemQtyList = []; //cart will be [123443:4,2323443:1,12345:43]
+    if (dev) print(userCartList);
+    for (int i = 1; i < userCartList!.length; i++) {
+      String item = userCartList[i]
+          .toString(); //this will get items in the cart as 122334:4
+
+     // var getItemQtyonly = item.split(':')[ 1];
+    // //this will split the items n select everything b4 ":" i.e 122334
+     // itemQtyList.add(getItemQtyonly);
+       
+      //another way
+        var lastCaracterPositionOfItemBeforeColon = item.lastIndexOf(":");
+      String getItemQtyOnly =
+          item.substring(lastCaracterPositionOfItemBeforeColon+1);
+      itemQtyList.add(getItemQtyOnly); 
+      if (dev) print("$itemQtyList ...... $i");
+    }
+
+    return itemQtyList;
   }
 }
