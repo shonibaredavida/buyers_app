d2f42b7e3b7710b3af95649a96759f0440fd0670
shodav4real <shonibaredavida@yahoo.com>
2023-02-03 15:44:20 +0100

    completed
    added link to home n orders page on the drawer

---
+++ lib/orders_screen/orders_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class OrdersScreen extends StatefulWidget {
+  const OrdersScreen({super.key});
+
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text(
+          "iShop",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Colors.green,
+                  Colors.black,
+                  Colors.red,
+                ],
+                begin: FractionalOffset(0.0, 0.0),
+                end: FractionalOffset(1.0, 0.0),
+                // stops: [0.0, 1.0],  #check
+                tileMode: TileMode.clamp),
+          ),
+        ),
+      ),
+      body: null,
+    );
+  }
+}
