655ccba7f0d418bc121aa0ed27f10ee7b6e413f9
shodav4real <shonibaredavida@yahoo.com>
2023-02-05 07:19:51 +0100

    Completed

--- lib/orders_screen/order_card.dart
+++ lib/orders_screen/order_card.dart
@@ -1,13 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:trial/global/global.dart';
+import 'package:trial/models/items_models.dart';
 
 class OrderCart extends StatefulWidget {
   OrderCart(
-      {this.data, this.itemCounts, this.orderId, this.separateQuantityList});
+      {this.data,
+      this.itemCounts,
+      this.orderId,
+      required this.separateQuantityList});
   int? itemCounts;
   List<DocumentSnapshot>? data;
   String? orderId;
-  List<String>? separateQuantityList;
+  List<String> separateQuantityList;
 
   @override
   State<OrderCart> createState() => _OrderCartState();
@@ -17,15 +22,103 @@ class _OrderCartState extends State<OrderCart> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.red,
+      color: Colors.black,
       elevation: 10,
-      shadowColor: Colors.white54,
+      shadowColor: Colors.white38,
       child: Container(
-        color: Colors.white54,
+        color: Colors.transparent,
         padding: EdgeInsets.all(10),
         margin: EdgeInsets.all(10),
         height: widget.itemCounts! * 125,
+        child: ListView.builder(
+            itemCount: widget.itemCounts,
+            itemBuilder: (context, index) {
+              Items model = Items.fromJson(
+                  widget.data![index].data() as Map<String, dynamic>);
+              return placedOrderItemsDesignWidget(
+                  model, context, widget.separateQuantityList[index]);
+            }),
       ),
     );
   }
+}
+
+Widget placedOrderItemsDesignWidget(
+    Items items, BuildContext context, itemQty) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    height: 120,
+    color: Colors.transparent,
+    child: Row(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(40),
+          child: Image.network(
+            items.thumbnailUrl.toString(),
+            width: 120,
+          ),
+        ),
+        sizedBox(width: 10),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                sizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        items.itemTitle.toString(),
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                    ),
+                    sizedBox(width: 10),
+                    const Text(
+                      "N",
+                      style: TextStyle(
+                          color: Colors.purpleAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                    Text(
+                      items.price.toString(),
+                      style: const TextStyle(
+                          color: Colors.purpleAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  ],
+                ),
+                sizedBox(height: 20),
+                Row(
+                  children: [
+                    const Text(
+                      "x",
+                      style: TextStyle(
+                        color: Colors.purpleAccent,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      itemQty,
+                      style: const TextStyle(
+                          color: Colors.purpleAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    ),
+  );
 }
